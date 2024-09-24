4004a3cf82ec674cde842e17e2a76b89d698a212
Gregory Gerard <wako057@gmail.com>
2024-09-24 12:03:50 +0200

    feat: setup basic search for future render props

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+
 export default function SearchableList({ items }) {
+    const [searchTerm, setSearchTerms] = useState('');
+
+    const searchResults = items.filter(item => 
+        JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()))
+
+    function handleChange(event) {
+        setSearchTerms(event.target.value);
+    }
+
     return (
         <div className="searchable-list">
-            <input type="search" placeholder="Search" />
+            <input type="search" placeholder="Search" onChange={handleChange}/>
             <ul>
-                {items.map((item, index) => (
+                {searchResults.map((item, index) => (
                     <li key={index}>
                         {item.toString()}
                     </li>
