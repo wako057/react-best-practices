45d7338c68f3791767e067856065556dcffa011a
Gregory Gerard <wako057@gmail.com>
2024-09-24 10:42:39 +0200

    feat: introduce compound component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+  <main>
+    <section>
+      <h2>Why work with us?</h2>
+      <Accordion className="accordion">
+      <AccordionItem className="accordion-item" title="We got 20 years of experience">
+          <article>
+            <p>You can&apost;t go wrong with us</p>
+            <p>
+              We are in the business of planning highly individualized 
+              vacation trips for more than 20 years.
+            </p>
+          </article>
+        </AccordionItem>
+        <AccordionItem className="accordion-item" title="We working with local guide">
+          <article>
+            <p>We are not doing this alone from our office.</p>
+            <p>
+              Instead, we are working with local guide to ensure a safe and 
+              pleasant vacation.
+            </p>
+          </article>
+        </AccordionItem>
+      </Accordion>
+    </section>
+  </main>
+  );
 }
 
 export default App;
