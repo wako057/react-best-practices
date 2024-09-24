76f39c4a1417c33349f059a7d9fde8897e8d511b
Gregory Gerard <wako057@gmail.com>
2024-09-24 10:56:34 +0200

    feat: managin multi-component state with Context

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ function App() {
     <section>
       <h2>Why work with us?</h2>
       <Accordion className="accordion">
-      <AccordionItem className="accordion-item" title="We got 20 years of experience">
+      <AccordionItem id="experience" className="accordion-item" title="We got 20 years of experience">
           <article>
-            <p>You can&apost;t go wrong with us</p>
+            <p>You can't go wrong with us</p>
             <p>
               We are in the business of planning highly individualized 
               vacation trips for more than 20 years.
             </p>
           </article>
         </AccordionItem>
-        <AccordionItem className="accordion-item" title="We working with local guide">
+        <AccordionItem id="local-guides" className="accordion-item" title="We working with local guide">
           <article>
             <p>We are not doing this alone from our office.</p>
             <p>
--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,10 +1,23 @@
 import { Children } from "react";
+import { useAccordionContext } from "./Accordion";
 
-export default function AccordionItem({className, title, children}) {
+export default function AccordionItem({ id, className, title, children }) {
+    const { openItemId, openItem, closeItem } = useAccordionContext();
+
+    const isOpen = openItemId === id;
+
+    function handleClick() {
+        if (isOpen) {
+            closeItem();
+        } else {
+            openItem(id);
+        }
+    }
+
     return (
         <li className={className}>
-            <h3>{title}</h3>
-            <div>{children}</div>
+            <h3 onClick={handleClick}>{title}</h3>
+            <div className={isOpen ? 'accordion-item-content open' : 'accordion-item-content'}>{children}</div>
         </li>
     );
 }
